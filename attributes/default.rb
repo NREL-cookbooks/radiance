c37365bf610c51701ad0b1ccf856f96483186871
Nicholas Long <nicholas.long@nrel.gov>
2014-02-27 07:17:34 -0700

    update default version

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,5 +7,5 @@
 # This uses the NREL mirror Github repo
 default['radiance']['install_method'] = "source"
 default['radiance']['source_url'] = "https://github.com/NREL/Radiance/archive" 
-default['radiance']['version'] = "4.2.a.1"
+default['radiance']['version'] = "4.2.a.2"
 default['radiance']['install_prefix'] = "/usr/local/radiance"
